46554883e3b68e8833df28c4d669faddcf5faa11
Jones <chaiy2004@163.com>
2019-12-27 15:55:08 +0800

    algorithms exercise 17 - quick sort return self

--- quick_sort.py
+++ quick_sort.py
@@ -25,3 +25,33 @@ def quick_sort2(seq) -> list:
         else:
             l_sub.append(item)
     return quick_sort2(l_sub) + [pivot] + quick_sort2(g_sub)
+
+
+def quick_sort3(seq):
+    """ quick sorted self """
+    q_sort(seq, 0, len(seq) - 1)
+
+
+def q_sort(seq, left_pos, right_pos):
+    left, right = left_pos, right_pos
+    if left >= right:
+        return
+    pivot = seq[left]
+    left += 1
+    while True:
+        while seq[left] < pivot and left <= right:
+            left += 1
+        while seq[right] > pivot and left <= right:
+            right -= 1
+        if left < right:
+            seq[left], seq[right] = seq[right], seq[left]
+        else:
+            break
+
+    # seq[left_pos](pivot),seq[right] exchange their value
+    seq[left_pos], seq[right] = seq[right], seq[left_pos]
+    # now seq[right]=pivot
+
+    # print(f'right:seq[{right}]={seq[right]}', seq)
+    q_sort(seq, left_pos, right - 1)
+    q_sort(seq, right + 1, right_pos)
