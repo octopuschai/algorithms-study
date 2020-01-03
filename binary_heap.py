36feb29ff18c765365245eb16be863129e1d4ad2
Jones <chaiy2004@163.com>
2020-01-03 16:11:49 +0800

    algorithms exercise 20 - binary heap revised

--- binary_heap.py
+++ binary_heap.py
@@ -2,11 +2,15 @@
 
 
 class BinaryHeap(object):
-    def __init__(self, ):
+    """ Binary Heap, top element (heap_list[1]) is an minimum element """
+    def __init__(self, seq=None):
         self.heap_list = [0]
         self.size = 0
+        if seq:
+            self.build(seq)
 
     def insert(self, value):
+        """ insert an element """
         self.heap_list.append(value)
         self.size += 1
         cur_idx = self.size
@@ -20,18 +24,28 @@ class BinaryHeap(object):
             cur_idx = par_idx
             par_idx = par_idx // 2
 
-    def del_min(self, ):
+    def pop(self, ):
+        """ pop the minimum element from Binary Heap """
         if self.size == 0:
             raise IndexError('Error, Binary Heap is empty!')
         else:
             res = self.heap_list[1]
-            tmp_list = self.heap_list[2:]
-            self.__init__()
-            for value in tmp_list:
-                self.insert(value)
+            self.heap_list[1] = self.heap_list[self.size]
+            self.heap_list.pop()
+            self.size -= 1
+            p = 1
+            while True:
+                n = self.min_child(p)
+                if n is not None and (self.heap_list[p] > self.heap_list[n]):
+                    self.heap_list[p], self.heap_list[n] = self.heap_list[
+                        n], self.heap_list[p]
+                    p = n
+                else:
+                    break
         return res
 
     def min_child(self, par_idx):
+        """ compare left child and right child, return min child or None """
         left = par_idx * 2
         right = left + 1
         if left > self.size:
@@ -42,3 +56,8 @@ class BinaryHeap(object):
             if self.heap_list[left] < self.heap_list[right]:
                 return left
             return right
+
+    def build(self, seq):
+        """ build a Binary Heap from a sequence """
+        for item in seq:
+            self.insert(item)
